4ec8d494ccdfaa7f0e5b936facaeea0fab2a38d9
vic-1998 <victor.mplaza98@gmail.com>
2021-08-10 17:42:10 -0500

    Minimum Operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+
+
+def minOperations(n):
+    """
+    Minimum Operations
+    """
+
+    if n <= 1:
+        return 0
+    num, div, numOfOperations = n, 2, 0
+
+    while num > 1:
+        if num % div == 0:
+            num = num / div
+            numOfOperations = numOfOperations + div
+        else:
+            div += 1
+    return numOfOperations
