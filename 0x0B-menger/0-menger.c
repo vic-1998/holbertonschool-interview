c65685f13a8bbe40907222138bdec34c49ef212a
vic-1998 <victor.mplaza98@gmail.com>
2021-10-05 16:31:37 -0500

    Menger sponge

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,34 @@
+#include "menger.h"
+
+/**
+ * menger - function that draws a 2D menger sponge
+ * @level: level of the menger sponge to draw
+ * Return: Void function
+ */
+
+void menger(int level)
+{
+int n, m, j, x, y;
+char s;
+m = pow(3, level);
+for (n = 0; n < m; n++)
+{
+for (j = 0; j < m;)
+{
+s = '#';
+x = n;
+y = j++;
+while (x > 0 || y > 0)
+{
+if (x % 3 == 1 && y % 3 == 1)
+{
+s = ' ';
+}
+x /= 3;
+y /= 3;
+}
+printf("%c", s);
+}
+printf("\n");
+}
+}
